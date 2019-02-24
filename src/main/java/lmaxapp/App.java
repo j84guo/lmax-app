52a463e0c29e1baecf19be2a1d55cf2c94c2faa7
Jackson Guo <jacguo@linkedin.com>
2019-02-24 00:46:27 -0800

    use 2 event handlers

--- src/main/java/lmaxapp/App.java
+++ src/main/java/lmaxapp/App.java
@@ -1,13 +1,24 @@
 package lmaxapp;
 
 import com.lmax.disruptor.BlockingWaitStrategy;
-import com.lmax.disruptor.RingBuffer;
 import com.lmax.disruptor.dsl.Disruptor;
 import com.lmax.disruptor.dsl.ProducerType;
 import com.lmax.disruptor.util.DaemonThreadFactory;
 import java.nio.ByteBuffer;
 
 
+/* Sequence numbers:
+   In the disruptor pattern, a RingBuffer tracks its next sequence number. Each consumer also tracks its own sequence
+   number. Since a sequence number is updated only by its owner, an important source of contention is removed
+
+   Reading:
+   Consumers ask what the highest sequence number is, then consume up to it without contention, since the writer is not
+   writing to those spots
+
+   Writing:
+   Phase 1 - Publisher determines the next available spot. RingBuffer determines if next slot is free by checking the
+             sequences of EventProcessors reading from it
+   Phase 2 - Write into slot, commit (causes RingBuffer to update sequence number) */
 public class App {
     public static void main(String[] args) throws InterruptedException {
         /* performance improves by specifying explicitly when we have a single writing thread */
@@ -19,12 +30,11 @@ public class App {
             new BlockingWaitStrategy());
 
         /* single consumer thread */
-        disruptor.handleEventsWith(new LongEventHandler());
+        disruptor.handleEventsWith(new LongEventHandler(), new LongEventHandler());
         disruptor.start();
 
         /* produce ByteBuffer in main thread (e.g. read from network) */
-        RingBuffer<LongEvent> buffer = disruptor.getRingBuffer();
-        LongEventProducer producer = new LongEventProducer(buffer);
+        LongEventProducer producer = new LongEventProducer(disruptor.getRingBuffer());
         ByteBuffer bytes = ByteBuffer.allocate(8);
         long value = 0;
         while (true) {
--- src/main/java/lmaxapp/LongEventHandler.java
+++ src/main/java/lmaxapp/LongEventHandler.java
@@ -3,9 +3,10 @@ package lmaxapp;
 import com.lmax.disruptor.EventHandler;
 
 
-/* Handle LongEvents objects */
+/* Handle LongEvent objects. Each handler thread will receive all events but may choose which to handle based on the
+   event's sequence number */
 public class LongEventHandler implements EventHandler<LongEvent> {
   public void onEvent(LongEvent event, long sequence, boolean endOfBatch) {
-    System.out.printf("event: %d\n", sequence);
+    System.out.printf("[%s] event: %d\n", Thread.currentThread().getName(), sequence);
   }
 }
